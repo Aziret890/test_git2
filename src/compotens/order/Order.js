3a5adbf09eb80155d5c3367ed229c37e43e5d67e
mairambek <mairambekjoldoshbekov30@gmail.com>
2024-02-25 15:59:44 +0600

    header added order

--- src/compotens/order/Order.js
+++ src/compotens/order/Order.js
@@ -4,25 +4,28 @@ import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
 
 function OrderDetals() {
-  const { basket } = useProvaider();
-  console.log(basket);
-  return (
-    <div className="container">
-      <div style={{ display: "flex", flexWrap: "wrap" }} className="blockMenu">
-        {basket?.map((el) => (
-          <Card
-            hoverable
-            style={{
-              width: 240,
-            }}
-            cover={<img alt="example" src={el.url} />}
-          >
-            <Meta title={el.name} description={el.price} />
-          </Card>
-        ))}
-      </div>
-    </div>
-  );
+    const { basket } = useProvaider();
+    console.log(basket);
+    return (
+        <div className="container">
+            <div
+                style={{ display: "flex", flexWrap: "wrap" }}
+                className="blockMenu"
+            >
+                {basket?.map((el) => (
+                    <Card
+                        hoverable
+                        style={{
+                            width: 240,
+                        }}
+                        cover={<img alt="example" src={el.url} />}
+                    >
+                        <Meta title={el.name} description={el.price} />
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default OrderDetals;
